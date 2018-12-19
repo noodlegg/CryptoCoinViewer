81c80a5de9e983242594cff37acb58ab1c102ce8
NoodleGG <TGian98@gmail.com>
2018-12-19 01:26:26 +0100

    Updates

---
+++ precache-manifest.19b8df060c5f3c77b11bfdca8bd8c1d2.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "d5084f7c42d6c7bd78d042845c8bea19",
+    "url": "/CryptoCoinViewer/static/media/btc_banner.d5084f7c.png"
+  },
+  {
+    "revision": "d8ab4df3c0d19c472c67",
+    "url": "/CryptoCoinViewer/static/css/main.80a78526.chunk.css"
+  },
+  {
+    "revision": "6ad735013470d333265464443bed4aef",
+    "url": "/CryptoCoinViewer/static/media/xlm_banner.6ad73501.png"
+  },
+  {
+    "revision": "97aaed95f0f571600277",
+    "url": "/CryptoCoinViewer/static/js/1.97aaed95.chunk.js"
+  },
+  {
+    "revision": "f9c939acc2a43526d575",
+    "url": "/CryptoCoinViewer/static/js/runtime~main.f9c939ac.js"
+  },
+  {
+    "revision": "d8ab4df3c0d19c472c67",
+    "url": "/CryptoCoinViewer/static/js/main.d8ab4df3.chunk.js"
+  },
+  {
+    "revision": "e6a2ba144f32aa7e67aead65e0fe82d2",
+    "url": "/CryptoCoinViewer/static/media/eth_banner.e6a2ba14.png"
+  },
+  {
+    "revision": "616d6ce33ee3739bef7367378ef0e8fd",
+    "url": "/CryptoCoinViewer/static/media/iota_banner.616d6ce3.png"
+  },
+  {
+    "revision": "c82f1e4b84ab697560bc81d5ce0b2da9",
+    "url": "/CryptoCoinViewer/static/media/xrp_banner.c82f1e4b.png"
+  },
+  {
+    "revision": "97aaed95f0f571600277",
+    "url": "/CryptoCoinViewer/static/css/1.35eeef3b.chunk.css"
+  },
+  {
+    "revision": "56cf1c7e30c1ba7180393c7bc0e14627",
+    "url": "/CryptoCoinViewer/index.html"
+  }
+];
